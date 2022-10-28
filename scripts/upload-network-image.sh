d8d7158b17fce2565a9de1fb71368ed9bbecc386
Gonéri Le Bouder <goneri@lebouder.net>
2022-10-28 11:14:51 -0400

    cisco c8000v appliance support viortio

--- scripts/upload-network-image.sh
+++ scripts/upload-network-image.sh
@@ -13,6 +13,11 @@ function upload() {
     if [[ ${image_name} == *"nexus9"* ]]; then
         properties="--property hw_vif_model=e1000 --property hw_disk_bus=sata --property hw_firmware_type=uefi"
     fi
+    if [[ ${image_name} == *"c8000v-universalk9"* ]]; then
+	# The appliance support virtio by default according to the documentation
+	# https://www.cisco.com/c/en/us/td/docs/routers/C8000V/Configuration/c8000v-installation-configuration-guide.pdf
+        properties=""
+    fi
 
     openstack image create --progress --disk-format qcow2 --file ${image_name}.qcow2 ${properties} ${image_name}
 
